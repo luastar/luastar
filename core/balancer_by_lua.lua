82345f6a81474ed80849ab6463dae2eba439f076
zhuminghua <19102630@163.com>
2025-04-10 20:49:34 +0800

    引入 jwt 和 负载均衡算法

---
+++ core/balancer_by_lua.lua
@@ -0,0 +1,8 @@
+local balancer = require "ngx.balancer"
+
+-- 使用 ngx.ctx 存放模块，再从字典中动态取服务器配置
+local ok, err = balancer.set_current_peer("127.0.0.1", 8002)
+if not ok then
+    ngx.log(ngx.ERR, "failed to set the current peer: ", err)
+    return ngx.exit(500)
+end
